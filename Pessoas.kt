8c4d26b3d23dd20f4989b4e6ba1d71902430000e
perkles <douglasribeirosanguino3@gmail.com>
2021-07-12 21:18:18 -0300

    Pessoas.kt

---
+++ Pessoas.kt
@@ -0,0 +1,11 @@
+package main.one.digitalinnovation
+
+class Pessoas {
+    val nome:String = "Jão"
+    val cpf:String = "111.222.333-14"
+}
+fun main(){
+    var Jão = Pessoas()
+    println(Jão.nome)
+    println(Jão.cpf)
+}
